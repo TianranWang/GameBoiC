82aa5dc87c131dfe3be6cb22ca6989f640cbac62
Tianran <tianran@TianrandeiMac.local>
2017-01-01 13:38:19 -0500

    Initial Commit

---
+++ GameBoiC/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameBoiC
+//
+//  Created by Tianran on 2017-01-01.
+//  Copyright © 2017 Tianran. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
